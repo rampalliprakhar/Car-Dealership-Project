c2c79d86e6e3e93c387c357a94913f50341e2072
TrinyN <122491610+TrinyN@users.noreply.github.com>
2023-02-04 14:18:58 -0500

    small update

--- GeneralManager.java
+++ GeneralManager.java
@@ -1,32 +1,21 @@
 
 public class GeneralManager extends Employee {
 	
-	/* unique responsibilities: 
+	/* unique view: 
 	 * can create new employee??
 	 * can access and approve records of sales
 	 * can access a report 
 	 * can apply discounts
 	 */
 	
-	
 	// default constructor
 	public GeneralManager() {
-		super();
+		System.out.println("error, please fill out the form");    // should change to pop up error or remind the user to fill out the rest of the form
 	}
 
 	// constructor
-	public GeneralManager(String firstName, String lastName, String position, String userName, String password) {
-		super(firstName, lastName, "General Manager", userName, password);
+	public GeneralManager(String firstName, String lastName, String password) {
+		super(firstName, lastName, "General Manager", password);
 	}
-	
-	public double setPercentageDiscount(double percentageDiscount, double originalPrice) {    	  // ex: 25% off 
-		return (100 - percentageDiscount) * originalPrice;
-	}
-	
-	public void setPriceDiscount(double priceDiscount, double originalPrice) {			  // ex: $2,500 off or set new price to 10,000??? 
-	
-	
-	}
-
 	
 }
