d68ec39e494332feb4e867bb349c0b7668797c54
TrinyN <122491610+TrinyN@users.noreply.github.com>
2023-02-04 14:21:01 -0500

    Update Salesperson.java

--- Salesperson.java
+++ Salesperson.java
@@ -2,9 +2,10 @@ import java.util.Date;
 
 public class Salesperson extends Employee{
 	
-	/* unique responsibilities: 
+	/* unique view: 
 	 * can create and change customer profiles
 	 * can create records of sales
+	 * can request approval of records of sales
 	 */
 	
 	
@@ -13,33 +14,12 @@ public class Salesperson extends Employee{
 	
 	// default constructor
 	public Salesperson() {
-		super();
+		System.out.println("error, please fill out the form");
 	}
 	
 	// constructor
-	public Salesperson(String firstName, String lastName, String position, String userName,
-			String password) {
+	public Salesperson(String firstName, String lastName, String password) {
 		
 		super(firstName, lastName, "Salesperson", userName, password);
 	}
-
-
-	public void createCustomerProfile(String firstName, String lastName, String phoneNum, Date birthDate,
-			Addresses address) {
-		CustomerProfile newProfile = new CustomerProfile(firstName, lastName, phoneNum, birthDate, address);
-
-	}
-	
-	
-	
-	
-//	private void createRecordOfSale(Vehicle VIN, CustomerProfile customerID, String paymentMethod) { // salesperson will give vehicle id, customer id, and payment method???
-//		
-//		
-//		
-//		RecordOfSale newRecord = new RecordOfSale(/* get todays date*/, vehicleSold /* get vehicle make model etc from vin*/, vehiclePrice, /* get price and calculate commission*/,
-//			paymentMethod, manufacturer, customerName /* get name from id*/, customerID, employeeID,
-//				employeeName/* get name from id*/);
-//		
-//	}
 }
