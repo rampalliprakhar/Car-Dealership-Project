a7b2b80f27adf21108f554cf330a00f16afbcdd9
Shakira Eaddy <Seaddy08@gmail.com>
2023-02-20 13:06:32 -0500

    Add files via upload

---
+++ SalespersonUIController.java
@@ -0,0 +1,49 @@
+package application;
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+public class SalespersonUIController {
+    @FXML
+    void btnCreOrdManuClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCreOrderDealClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEntPurchClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRecNewArrivClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegCustClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignOutClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnVerTradValClicked(ActionEvent event) {
+
+    }
+}
