7cb4a1f0190eacd17640cc7757e7806678d91942
Shakira Eaddy <Seaddy08@gmail.com>
2023-02-26 23:54:42 -0500

    Add files via upload

--- ManagerViewUIController.java
+++ ManagerViewUIController.java
@@ -1,66 +1,113 @@
 package application;
-import javafx.event.ActionEvent;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.*;
 import javafx.scene.Parent;
-
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.io.IOException;
 
 public class ManagerViewUIController {
 	
+	@FXML
+    private Button CreateOrderDealership;
+
     @FXML
-    void btnCreOrdManuClicked(ActionEvent event) {
+    private Button CreateOrderManufacturer;
+
+    @FXML
+    private Button EnterPurchase;
+
+    @FXML
+    private Button RecordNewArrival;
+
+    @FXML
+    private Button RegisterCustomer;
+
+    @FXML
+    private Button SetWeeklyDiscounts;
+
+    @FXML
+    private Button SignOut;
+
+    @FXML
+    private Button VerifyTadeValue;
+
+    @FXML
+    private Button ViewWeeklyReports;
+
+    public void btnCreOrdManuClicked(ActionEvent event) throws IOException{
+    	// goes to Create Order for Manufacturer page.
+    	if(event.getSource() == CreateOrderManufacturer) {
+    		 Main m = new Main();
+    		 m.changeScene("CreateOrderforManufacturer.fxml");
+    	}      
+    }
+    
+    public void btnCreOrderDealClicked(ActionEvent event) throws IOException {
+    	// goes to Create Order for Other Dealership page.
+		 if(event.getSource() == CreateOrderDealership) {
+			 Main m = new Main();
+			 m.changeScene("CreateOrderforOtherDealership.fxml");
+		 }
 
     }
 
-    @FXML
-    void btnCreOrderDealClicked(ActionEvent event) {
-
+    public void btnEntPurchClicked(ActionEvent event) throws IOException {
+     // goes to Enter Purchase page.
+    	if(event.getSource() == EnterPurchase) {
+    	    Main m = new Main();
+    	    m.changeScene("PurchaseVehicle1.fxml");
+    	}
     }
 
-    @FXML
-    void btnEntPurchClicked(ActionEvent event) {
-
+    public void btnRecNewArrivClicked(ActionEvent event) throws IOException {
+    	// goes to Record New Arrival page.
+		 if(event.getSource() == RecordNewArrival) {
+			 Main m = new Main();
+			 m.changeScene("RecordVehicleUI.fxml");
+		 }
     }
 
-    @FXML
-    void btnRecNewArrivClicked(ActionEvent event) {
-
+    public void btnRegCustClicked(ActionEvent event) throws IOException {
+    	// goes to Register Customer page.
+    	if(event.getSource() == RegisterCustomer) {
+    	    Main m = new Main();
+    		m.changeScene("RegCustomerUI.fxml");
+    	}
     }
 
-    @FXML
-    void btnRegCustClicked(ActionEvent event) {
-
+    public void btnSetWeekDiscClicked(ActionEvent event) throws IOException {
+    	// goes to Set Weekly Discounts page.
+    	if(event.getSource() == SetWeeklyDiscounts) {
+    	    Main m = new Main();
+    		m.changeScene("SetDiscountsUI.fxml");
+    	}
     }
 
-    @FXML
-    void btnSetWeekDiscClicked(ActionEvent event) {
-
+    public void btnSignOutClicked(ActionEvent event) throws IOException {
+    	// goes to Sign Out page.
+    	if(event.getSource() == SignOut) {
+    	    Main m = new Main();
+    		m.changeScene("CarLogout.fxml");
+    	}
     }
 
-    @FXML
-    void btnSignOutClicked(ActionEvent event) {
-
+    public void btnVerTradValClicked(ActionEvent event) throws IOException {
+    	// goes to Verify Trade-In Value page.
+    	// Had to change the names in the original files.
+    	if(event.getSource() == VerifyTadeValue) {
+    		 Main m = new Main();
+    		 m.changeScene("VerifyTradeIn.fxml");
+    	}
     }
 
-    @FXML
-    void btnVerTradValClicked(ActionEvent event) {
-
+    public void btnViewWeekRepClicked(ActionEvent event) throws IOException {
+    	// goes to View Weekly Reports page.
+    	// In progress.
+    	if(event.getSource() == ViewWeeklyReports) {
+    	    Main m = new Main();
+    		m.changeScene("");
+    	}
     }
-
-    @FXML
-    void btnViewWeekRepClicked(ActionEvent event) {
-
-    }
-
 }
-
