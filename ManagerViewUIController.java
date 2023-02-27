d1d5012b46ce50f0abdd31d43ff071c01971cd78
Shakira Eaddy <Seaddy08@gmail.com>
2023-02-26 23:58:07 -0500

    Update ManagerViewUIController.java

--- ManagerViewUIController.java
+++ ManagerViewUIController.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class ManagerViewUIController {
 	
-	@FXML
+    @FXML
     private Button CreateOrderDealership;
 
     @FXML
@@ -39,22 +39,21 @@ public class ManagerViewUIController {
     public void btnCreOrdManuClicked(ActionEvent event) throws IOException{
     	// goes to Create Order for Manufacturer page.
     	if(event.getSource() == CreateOrderManufacturer) {
-    		 Main m = new Main();
-    		 m.changeScene("CreateOrderforManufacturer.fxml");
+            Main m = new Main();
+    	    m.changeScene("CreateOrderforManufacturer.fxml");
     	}      
     }
     
     public void btnCreOrderDealClicked(ActionEvent event) throws IOException {
     	// goes to Create Order for Other Dealership page.
-		 if(event.getSource() == CreateOrderDealership) {
-			 Main m = new Main();
-			 m.changeScene("CreateOrderforOtherDealership.fxml");
-		 }
-
+	if(event.getSource() == CreateOrderDealership) {
+	    Main m = new Main();
+	    m.changeScene("CreateOrderforOtherDealership.fxml");
+	}
     }
 
     public void btnEntPurchClicked(ActionEvent event) throws IOException {
-     // goes to Enter Purchase page.
+        // goes to Enter Purchase page.
     	if(event.getSource() == EnterPurchase) {
     	    Main m = new Main();
     	    m.changeScene("PurchaseVehicle1.fxml");
@@ -63,17 +62,17 @@ public class ManagerViewUIController {
 
     public void btnRecNewArrivClicked(ActionEvent event) throws IOException {
     	// goes to Record New Arrival page.
-		 if(event.getSource() == RecordNewArrival) {
-			 Main m = new Main();
-			 m.changeScene("RecordVehicleUI.fxml");
-		 }
+	if(event.getSource() == RecordNewArrival) {
+	    Main m = new Main();
+	    m.changeScene("RecordVehicleUI.fxml");
+	}
     }
 
     public void btnRegCustClicked(ActionEvent event) throws IOException {
     	// goes to Register Customer page.
     	if(event.getSource() == RegisterCustomer) {
     	    Main m = new Main();
-    		m.changeScene("RegCustomerUI.fxml");
+    	    m.changeScene("RegCustomerUI.fxml");
     	}
     }
 
@@ -81,7 +80,7 @@ public class ManagerViewUIController {
     	// goes to Set Weekly Discounts page.
     	if(event.getSource() == SetWeeklyDiscounts) {
     	    Main m = new Main();
-    		m.changeScene("SetDiscountsUI.fxml");
+    	    m.changeScene("SetDiscountsUI.fxml");
     	}
     }
 
@@ -89,7 +88,7 @@ public class ManagerViewUIController {
     	// goes to Sign Out page.
     	if(event.getSource() == SignOut) {
     	    Main m = new Main();
-    		m.changeScene("CarLogout.fxml");
+    	    m.changeScene("CarLogout.fxml");
     	}
     }
 
@@ -97,8 +96,8 @@ public class ManagerViewUIController {
     	// goes to Verify Trade-In Value page.
     	// Had to change the names in the original files.
     	if(event.getSource() == VerifyTadeValue) {
-    		 Main m = new Main();
-    		 m.changeScene("VerifyTradeIn.fxml");
+    	    Main m = new Main();
+    	    m.changeScene("VerifyTradeIn.fxml");
     	}
     }
 
@@ -107,7 +106,7 @@ public class ManagerViewUIController {
     	// In progress.
     	if(event.getSource() == ViewWeeklyReports) {
     	    Main m = new Main();
-    		m.changeScene("");
+    	    m.changeScene("");
     	}
     }
 }
