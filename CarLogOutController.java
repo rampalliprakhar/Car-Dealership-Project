2519ea88fec0a6f94e2d08e842c00ed2b971fad7
AtlanticRim <114529909+AtlanticRim@users.noreply.github.com>
2023-03-17 23:56:07 -0400

    Bug fix to close application on logout

--- CarLogOutController.java
+++ CarLogOutController.java
@@ -6,28 +6,33 @@ import javafx.scene.*;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import java.io.IOException;
 public class CarLogOutController {
-	 @ FXML
-	 private Button LogoutButton;
-	 private Stage stage;
-	 private Scene scene;
-	 private Parent root;
-	
-	 public void LogOut(ActionEvent event) throws IOException {
-		 try {
-			root = FXMLLoader.load(getClass().getResource("CarLogout.fxml"));
-			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-			scene = new Scene(root);
-			stage.setScene(scene);
-			stage.show();	
-		    LogoutButton.setOnAction(e -> {
-			  e.consume();
-		    });	
-		 } catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+     @ FXML
+     private Button LogoutButton;
+     private Stage stage;
+     private Scene scene;
+     private Parent root;
+    
+     public void LogOut(ActionEvent event) throws IOException {
+         Platform.exit();
+         
+         
+         
+//         try {
+//            root = FXMLLoader.load(getClass().getResource("CarLogout.fxml"));
+//            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//            scene = new Scene(root);
+//            stage.setScene(scene);
+//            stage.show();   
+//            LogoutButton.setOnAction(e -> {
+//              e.consume();
+//            }); 
+//         } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+    }
  
 }
