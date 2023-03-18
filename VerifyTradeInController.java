2fcbbb6be89b8c117673ad0e86bacc6742ecc514
AtlanticRim <114529909+AtlanticRim@users.noreply.github.com>
2023-03-18 00:35:40 -0400

    Updates to make Verify Trade In functional

--- VerifyTradeInController.java
+++ VerifyTradeInController.java
@@ -1,5 +1,93 @@
 package application;
 
-public class SampleController {
-	
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class VerifyTradeInController {
+
+    @FXML
+    private TextField YearField;
+    @FXML
+    private ChoiceBox<String> MakeDropdown;
+    @FXML
+    private TextField ModelField;
+    @FXML
+    private TextField MileageField;
+    @FXML
+    private TextField ColorField;
+    @FXML
+    private ChoiceBox<String> BodyConDropdown;
+    @FXML
+    private ChoiceBox<String> MechConDropdown;
+    @FXML
+    private Button SaveButton;    
+    @FXML
+    private Button ClearButton;
+    @FXML
+    private Button ReturnButton;
+    @FXML
+    private TextField ValueField;
+    
+    final private ObservableList<String> conditionList = FXCollections.observableArrayList("New", "Excellent", "Good", "Average", "Fair", "Poor", "Broken");
+    final private ObservableList<String> makeList = FXCollections.observableArrayList("Acura", "Alfa Romeo", "Aston Martin", "Audi", "Bentley", "BMW", "Buick", 
+            "Cadillac", "Chevrolet", "Chrysler", "Dodge", "Ferrari", "Fiat", "Ford", "Genesis", "GMC", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Kia", 
+            "Lamborghini", "Land Rover", "Lexus", "Lincoln", "Maserati", "Mazda", "McLaren", "Mercedes-Benz", "Mini", "Mitsubishi", "Nissan", "Porsche", "Ram", 
+            "Rolls-Royce", "Subaru", "Tesla", "Toyota", "Volkswagen", "Volvo");
+    
+    private String previousPage = Main.getView();
+    
+    @FXML
+    private void initialize() {
+        
+        /* This method is called automatically, and initializes the dropdown
+         * boxes with the values of the source ObservableLists*/
+        
+        MakeDropdown.setValue("Select a Make");
+        MakeDropdown.setItems(makeList);
+        
+        BodyConDropdown.setValue("Select a Condition");
+        BodyConDropdown.setItems(conditionList);
+        MechConDropdown.setValue("Select a Condition");
+        MechConDropdown.setItems(conditionList);
+        
+    } // end initialize method
+    
+    public void getValue(ActionEvent event) {
+        
+        // ADDITIONAL CODE NEEDED
+        setValue(123456.78);
+        
+    } // end getValue
+    
+    public void setValue(Double value) {
+        this.ValueField.setText("$ " + value);
+    } // end setValue
+    
+    public void clear(ActionEvent event) {
+        
+        YearField.clear();
+        MakeDropdown.setValue("Select a Make");
+        ModelField.clear();
+        MileageField.clear();
+        ColorField.clear();
+        BodyConDropdown.setValue("Select a Condition");
+        MechConDropdown.setValue("Select a Condition");
+        ValueField.clear();
+        
+    }
+    
+    public void pageReturn(ActionEvent event) throws IOException {
+            
+        Main m = new Main();
+        m.changeScene(previousPage);
+            
+    } // end pageReturn
+    
 }
