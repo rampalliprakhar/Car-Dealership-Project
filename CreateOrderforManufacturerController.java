56da0f746a0029ca982a860d0607da1e3723c551
rampalliprakhar <114451266+rampalliprakhar@users.noreply.github.com>
2023-03-19 08:50:46 -0400

    Updated Version of Manufacturer Controller

--- CreateOrderforManufacturerController.java
+++ CreateOrderforManufacturerController.java
@@ -1,12 +1,11 @@
 package application;
-
-import javafx.fxml.FXML;
 //import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 //import javafx.scene.Parent;
-import javafx.scene.control.*;
 //import javafx.scene.*;
 //import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.event.ActionEvent;
 import javafx.collections.*;
 import java.io.IOException;
@@ -15,73 +14,110 @@ import java.util.ResourceBundle;
 
 public class CreateOrderforManufacturerController implements Initializable {
 
-	 @FXML
-	 private ChoiceBox<String> MakeCar;
-	 final private ObservableList<String> makeList = FXCollections.observableArrayList("Acura", "Alfa Romeo", "Aston Martin", "Audi", "Bentley", "BMW", "Buick", 
-	            "Cadillac", "Chevrolet", "Chrysler", "Dodge", "Ferrari", "Fiat", "Ford", "Genesis", "GMC", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Kia", 
-	            "Lamborghini", "Land Rover", "Lexus", "Lincoln", "Maserati", "Mazda", "McLaren", "Mercedes-Benz", "Mini", "Mitsubishi", "Nissan", "Porsche", "Ram", 
-	            "Rolls-Royce", "Subaru", "Tesla", "Toyota", "Volkswagen", "Volvo");
+	@FXML
+	private ChoiceBox<String> MakeCar;
+	final private ObservableList<String> makeList = FXCollections.observableArrayList("Acura", "Alfa Romeo", "Aston Martin", "Audi", "Bentley", "BMW", "Buick", 
+	           "Cadillac", "Chevrolet", "Chrysler", "Dodge", "Ferrari", "Fiat", "Ford", "Genesis", "GMC", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Kia", 
+	           "Lamborghini", "Land Rover", "Lexus", "Lincoln", "Maserati", "Mazda", "McLaren", "Mercedes-Benz", "Mini", "Mitsubishi", "Nissan", "Porsche", "Ram", 
+	           "Rolls-Royce", "Subaru", "Tesla", "Toyota", "Volkswagen", "Volvo");
 	 
-	 @FXML
-	 private ChoiceBox<String> ModelCar;
-	 final private ObservableList<String> modelList = FXCollections.observableArrayList("Integra", "Giulia", "Vantage", "Q5", "Bentayga Hybrid", "X1", "Encore", 
-			    "Escalade", "Camaro", "Pacifica", "Charger", "Roma", "500X", "Ranger", "G80", "Terrain", "Civic", "Elantra", "Q60", "XF", "Wrangler", "Sportage", 
-			    "Urus", "Range Rover", "GX", "Aviator", "Grecale", "Tribute Hybrid", "720S", "A-Class", "Cooper", "Outlander", "Pathfinder", "Boxster", "ProMaster City", 
-			    "Phantom", "Ascent", "Model Y", "Camry", "Tiguan", "XC40");
+	@FXML
+	private TextField ModelCar;
 	 
-	 @FXML
-	 private Button ReturnButton;
+	@FXML
+	private Button ReturnButton;
 	 
-	 @FXML
-	 private Button SendButton;
+	@FXML
+	private Button SendButton;
 	 
-	 @FXML
-	 private Button ClearButton;
+	@FXML
+	private Button ClearButton;
 	 
-	 @FXML
-	 private TextField OrderID;
+	@FXML
+	private TextField OrderID;
 	 
-	 @FXML
-	 private TextField Year;
+	@FXML
+	private TextField Year;
 	 
-	 @FXML
-	 private TextField Color;
+	@FXML
+	private TextField Color;
 	 
-	 private String previousPage = "SalespersonView.fxml";
+	@FXML
+	private Label invalidInput;
 	 
-	 public void clear(ActionEvent event) {
+	protected String successPrompt = String.format("-fx-text-fill: GREEN;"); // correct input with green font
+	protected String failurePrompt = String.format("-fx-text-fill: RED;"); // incorrect input with red font
+
+	private String previousPage = "SampleSalesperson.fxml";
+
+  	public static boolean YearValidator(String yearinput) {
+        return yearinput.matches("^(19|20)[0-9][0-9]$"); // yearinput matches with regular expression
+	}
+	
+	public void clear(ActionEvent event) {
 		// checks the functionality of the button and resets the input field
-		if(event.getSource() == ClearButton) {
-			 OrderID.setText("");
-			 Year.setText("");
-			 Color.setText("");
-			 MakeCar.setValue("Select a Make");
-			 ModelCar.setValue("Select a Model");
+		 OrderID.clear();
+		 Year.clear();
+		 Color.clear();
+		 MakeCar.setValue("Select a Make");
+		 ModelCar.clear();
+	}
+	 
+	public void send(ActionEvent event) throws IOException {
+		// send the filled information to the VerifyTradeInUI
+		String order = OrderID.getText();
+		String year = Year.getText();
+		String model = ModelCar.getText();
+		String color = Color.getText();
+		if(order.isBlank() && year.isBlank() && model.isBlank() && color.isBlank()) {
+			invalidInput.setText("Login fields required!");
+			OrderID.setStyle(failurePrompt);
+			Year.setStyle(failurePrompt);
+			ModelCar.setStyle(failurePrompt);
+			Color.setStyle(failurePrompt);
 		}
-	 }
+		else 
+		{   // if orderNumber is blank
+			if(order.isBlank()) 
+			{
+				OrderID.setStyle(failurePrompt);
+				invalidInput.setText("Password required!");
+				OrderID.setStyle(successPrompt);
+			}
+			else 
+			{
+				if(model.isBlank()) {
+					ModelCar.setStyle(failurePrompt);
+					invalidInput.setText("Model required!");
+				}
+				if(YearValidator(year) == true) {
+	 		 	    if(year.length() > 4){ // checks whether the input is greater than 4
+	 		 	        invalidInput.setText("Year not greater than 4 digits!");
+	 		 	    	Year.setStyle(failurePrompt);
+	 		 	    }
+	 			}		
+		 		if(!color.matches("^[a-zA-Z]$") && !order.matches("^[0-9]{4}$")){
+		 			invalidInput.setStyle(failurePrompt);
+		 			invalidInput.setText("Wrong input");
+		 		}
+		 		if(!model.matches("^[a-zA-z0-9]$")) {
+		 			invalidInput.setText("Wrong input");
+		 		}
+		    }
+			Main m = new Main();
+			m.changeScene(Main.getView());
+		}
+	}
 	 
-	 public void send(ActionEvent event) throws IOException {
-		 // send the filled information to the VerifyTradeInUI
-		 MakeCar.getValue();
-		 ModelCar.getValue();
-		 Main m = new Main();
-		 m.changeScene("VerifyTradeIn.fxml");
-	 }
-	 
-	 public void returnPage(ActionEvent event) throws IOException {
-		 // returns back to the main page
-		 if(event.getSource() == ReturnButton) {
-			 Main m = new Main();
-			 m.changeScene(previousPage);
-		 }
-	 }
+	public void returnPage(ActionEvent event) throws IOException {
+	 // returns back to the main page
+		Main m = new Main();
+	 	m.changeScene(previousPage);
+	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
         MakeCar.setValue("Select a Make");
         MakeCar.getItems().addAll(makeList);
-        
-        ModelCar.setValue("Select a Model");
-        ModelCar.getItems().addAll(modelList);
 	}
 }
