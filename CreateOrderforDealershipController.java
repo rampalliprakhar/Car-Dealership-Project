d06b3f59f33cb8f149e4719e757e39bfbc20c7dc
rampalliprakhar <114451266+rampalliprakhar@users.noreply.github.com>
2023-03-30 09:41:49 -0400

    Updated Version

--- CreateOrderforDealershipController.java
+++ CreateOrderforDealershipController.java
@@ -7,64 +7,57 @@ import javafx.collections.*;
 import java.io.IOException;
 public class CreateOrderforDealershipController {
 
-	 private String previousPage = "SalespersonView.fxml";
-	 @FXML
-	 private Button ClearButton;
-	 @FXML
-	 private Button SendButton;
-	 @FXML
-	 private Button ReturnButton;
-	 @FXML
-	 private ChoiceBox<String> MakeCar;
+	private String previousPage = "Main.getView()";
+	@FXML
+	private Button ClearButton;
+	@FXML
+	private Button SendButton;
+	@FXML
+	private Button ReturnButton;
+	@FXML
+	private ChoiceBox<String> MakeCar;
 
-	 final private ObservableList<String> makeList = FXCollections.observableArrayList("Acura", "Alfa Romeo", "Aston Martin", "Audi", "Bentley", "BMW", "Buick", 
-            "Cadillac", "Chevrolet", "Chrysler", "Dodge", "Ferrari", "Fiat", "Ford", "Genesis", "GMC", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Kia", 
-            "Lamborghini", "Land Rover", "Lexus", "Lincoln", "Maserati", "Mazda", "McLaren", "Mercedes-Benz", "Mini", "Mitsubishi", "Nissan", "Porsche", "Ram", 
-            "Rolls-Royce", "Subaru", "Tesla", "Toyota", "Volkswagen", "Volvo");
+	final private ObservableList<String> makeList = FXCollections.observableArrayList("Acura", "Alfa Romeo", "Aston Martin", "Audi", "Bentley", "BMW", "Buick", 
+           "Cadillac", "Chevrolet", "Chrysler", "Dodge", "Ferrari", "Fiat", "Ford", "Genesis", "GMC", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Kia", 
+           "Lamborghini", "Land Rover", "Lexus", "Lincoln", "Maserati", "Mazda", "McLaren", "Mercedes-Benz", "Mini", "Mitsubishi", "Nissan", "Porsche", "Ram", 
+           "Rolls-Royce", "Subaru", "Tesla", "Toyota", "Volkswagen", "Volvo");
 	 
-	 @FXML
-	 private ChoiceBox<String> ModelCar;
-	 final private ObservableList<String> modelList = FXCollections.observableArrayList("Integra", "Giulia", "Vantage", "Q5", "Bentayga Hybrid", "X1", "Encore", 
-	    "Escalade", "Camaro", "Pacifica", "Charger", "Roma", "500X", "Ranger", "G80", "Terrain", "Civic", "Elantra", "Q60", "XF", "Wrangler", "Sportage", 
-	    "Urus", "Range Rover", "GX", "Aviator", "Grecale", "Tribute Hybrid", "720S", "A-Class", "Cooper", "Outlander", "Pathfinder", "Boxster", "ProMaster City", 
-	    "Phantom", "Ascent", "Model Y", "Camry", "Tiguan", "XC40");
+	@FXML
+	private TextField ModelCar;
 	 
-	 @FXML
-	 private ChoiceBox<String> bodyCondition;
-	 	final private ObservableList<String> bodyconditionList = FXCollections.observableArrayList("New", "Excellent", "Good", "Average", "Fair", "Poor", "Broken");
+	@FXML
+	private ChoiceBox<String> bodyCondition;
+		final private ObservableList<String> bodyconditionList = FXCollections.observableArrayList("New", "Excellent", "Good", "Average", "Fair", "Poor", "Broken");
 	 
-	 @FXML
-	 private ChoiceBox<String> mechCondition;
-	     final private ObservableList<String> mechconditionList = FXCollections.observableArrayList("New", "Excellent", "Good", "Average", "Fair", "Poor", "Broken");
+	@FXML
+	private ChoiceBox<String> mechCondition;
+	    final private ObservableList<String> mechconditionList = FXCollections.observableArrayList("New", "Excellent", "Good", "Average", "Fair", "Poor", "Broken");
 	 
-	 @FXML
-	 private TextField dealershipEntry;
+	@FXML
+	private TextField dealershipEntry;
 	 
-	 @FXML
-	 private TextField orderNumber;
+	@FXML
+	private TextField orderNumber;
 	 
-	 @FXML
-	 private TextField mileageText;
+	@FXML
+	private TextField mileageText;
 	 
-	 @FXML
-	 private TextField Year;
+	@FXML
+	private TextField Year;
 	 
-	 @FXML
-	 private TextField Color;
+	@FXML
+	private TextField Color;
+	
+	@FXML
+	private Label invalidPrompt;
 	 
-	 @FXML
-	 private Label invalidPrompt;
+	protected String successPrompt = String.format("-fx-text-fill: GREEN;"); // correct input with green font
+	protected String failurePrompt = String.format("-fx-text-fill: RED;"); // incorrect input with red font
 	 
-	 protected String successPrompt = String.format("-fx-text-fill: GREEN;"); // correct input with green font
-	 protected String failurePrompt = String.format("-fx-text-fill: RED;"); // incorrect input with red font
-	 
- 	 @FXML
-     private void initialize() {
+ 	@FXML
+    private void initialize() {
         MakeCar.setValue("Select a Make");
         MakeCar.getItems().addAll(makeList);
-        
-        ModelCar.setValue("Select a Model");
-        ModelCar.getItems().addAll(modelList);
         
         bodyCondition.setValue("Select condition");
         bodyCondition.getItems().addAll(bodyconditionList);
@@ -86,7 +79,7 @@ public class CreateOrderforDealershipController {
 		 Color.clear();
 		 mileageText.clear();
 	 	 MakeCar.setValue("Select a Make");
-	     ModelCar.setValue("Select a Model");
+	     ModelCar.clear();
 	     bodyCondition.setValue("Select condition");
 	     mechCondition.setValue("Select condition");
 	}
@@ -96,16 +89,18 @@ public class CreateOrderforDealershipController {
 		 String order = orderNumber.getText();
 		 String mileage = mileageText.getText();
 		 String year = Year.getText();
+		 String model = ModelCar.getText();
 		 String color = Color.getText();
 		// After filling information, it goes to the next page,
 		// where different information are filled
-		if(dealership.isBlank() && order.isBlank() && mileage.isBlank() && year.isBlank() && color.isBlank()) 
+		if(dealership.isBlank() && order.isBlank() && mileage.isBlank() && year.isBlank() && model.isBlank() && color.isBlank()) 
 		 {
 			 invalidPrompt.setText("Login fields required");
 			 dealershipEntry.setStyle(failurePrompt);
 			 orderNumber.setStyle(failurePrompt);
 			 mileageText.setStyle(failurePrompt);
 			 Year.setStyle(failurePrompt);
+			 ModelCar.setStyle(failurePrompt);
 			 Color.setStyle(failurePrompt);
 		 }
 		 else 
@@ -130,6 +125,9 @@ public class CreateOrderforDealershipController {
 		 				Main m = new Main();
 		 				m.changeScene(Main.getView());
 		 			}		
+			 		if(!model.matches("^[a-zA-z0-9]$")) {
+			 			invalidPrompt.setText("Wrong input");
+			 		}
 			 		if(!color.matches("^[a-zA-Z]$") && !dealership.matches("^[a-zA-Z]$")){
 			 			invalidPrompt.setText("Wrong input");
 			 		}
