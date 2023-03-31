80c89eadfc04ac8dd5d7b33a10faf4f7f365610b
AtlanticRim <114529909+AtlanticRim@users.noreply.github.com>
2023-03-30 21:29:40 -0400

    Update to 7 panes

--- SetDiscountsController.java
+++ SetDiscountsController.java
@@ -37,7 +37,10 @@ public class SetDiscountsController {
     private Vehicle vehicle1 = null;
     private Vehicle vehicle2 = null;
     private Vehicle vehicle3 = null;
-    // Expand to 7
+    private Vehicle vehicle4 = null;
+    private Vehicle vehicle5 = null;
+    private Vehicle vehicle6 = null;
+    private Vehicle vehicle7 = null;
     
     @FXML
     TabPane tabPane = new TabPane();
@@ -47,7 +50,17 @@ public class SetDiscountsController {
     Tab v2 = new Tab("Vehicle 2");
     @FXML
     Tab v3 = new Tab("Vehicle 3");
+    @FXML
+    Tab v4 = new Tab("Vehicle 4");
+    @FXML
+    Tab v5 = new Tab("Vehicle 5");
+    @FXML
+    Tab v6 = new Tab("Vehicle 6");
+    @FXML
+    Tab v7 = new Tab("Vehicle 7");
      
+    @FXML
+    private TextField v1TimeLot;
     @FXML
     private TextField v1ValueField;
     @FXML
@@ -70,6 +83,8 @@ public class SetDiscountsController {
     private Button v1Mark;
     
     @FXML
+    private TextField v1TimeLot1;
+    @FXML
     private TextField v1ValueField1;
     @FXML
     private TextField v1VINField1;
@@ -91,6 +106,8 @@ public class SetDiscountsController {
     private Button v1Mark1;
     
     @FXML
+    private TextField v1TimeLot2;
+    @FXML
     private TextField v1ValueField2;
     @FXML
     private TextField v1VINField2;
@@ -111,7 +128,98 @@ public class SetDiscountsController {
     @FXML
     private Button v1Mark2;
     
-    // Once this is okayed, I'll expand to 7 vehicles
+    @FXML
+    private TextField v1TimeLot3;
+    @FXML
+    private TextField v1ValueField3;
+    @FXML
+    private TextField v1VINField3;
+    @FXML
+    private TextField v1YearField3;
+    @FXML
+    private TextField v1Make3;
+    @FXML
+    private TextField v1ModelField3;
+    @FXML
+    private TextField v1MileageField3;
+    @FXML
+    private TextField v1ColorField3;
+    @FXML
+    private TextField v1BodyCon3;
+    @FXML
+    private TextField v1MechCon3;
+    @FXML
+    private Button v1Mark3;
+    
+    @FXML
+    private TextField v1TimeLot4;
+    @FXML
+    private TextField v1ValueField4;
+    @FXML
+    private TextField v1VINField4;
+    @FXML
+    private TextField v1YearField4;
+    @FXML
+    private TextField v1Make4;
+    @FXML
+    private TextField v1ModelField4;
+    @FXML
+    private TextField v1MileageField4;
+    @FXML
+    private TextField v1ColorField4;
+    @FXML
+    private TextField v1BodyCon4;
+    @FXML
+    private TextField v1MechCon4;
+    @FXML
+    private Button v1Mark4;
+    
+    @FXML
+    private TextField v1TimeLot5;
+    @FXML
+    private TextField v1ValueField5;
+    @FXML
+    private TextField v1VINField5;
+    @FXML
+    private TextField v1YearField5;
+    @FXML
+    private TextField v1Make5;
+    @FXML
+    private TextField v1ModelField5;
+    @FXML
+    private TextField v1MileageField5;
+    @FXML
+    private TextField v1ColorField5;
+    @FXML
+    private TextField v1BodyCon5;
+    @FXML
+    private TextField v1MechCon5;
+    @FXML
+    private Button v1Mark5;
+    
+    @FXML
+    private TextField v1TimeLot6;
+    @FXML
+    private TextField v1ValueField6;
+    @FXML
+    private TextField v1VINField6;
+    @FXML
+    private TextField v1YearField6;
+    @FXML
+    private TextField v1Make6;
+    @FXML
+    private TextField v1ModelField6;
+    @FXML
+    private TextField v1MileageField6;
+    @FXML
+    private TextField v1ColorField6;
+    @FXML
+    private TextField v1BodyCon6;
+    @FXML
+    private TextField v1MechCon6;
+    @FXML
+    private Button v1Mark6;
+    
     
     @FXML
     private void initialize() {
@@ -120,6 +228,7 @@ public class SetDiscountsController {
         setVehicles();
         
         if (vehicle1 != null) {
+            v1TimeLot.setText(vehicle1.getTimeOnLot().toString());
             v1ValueField.setText(vehicle1.getValue().toString());
             v1VINField.setText(vehicle1.getVIN());
             v1YearField.setText(vehicle1.getYear().toString());
@@ -132,6 +241,7 @@ public class SetDiscountsController {
         }
         
         if (vehicle2 != null) {
+            v1TimeLot1.setText(vehicle2.getTimeOnLot().toString());
             v1ValueField1.setText(vehicle2.getValue().toString());
             v1VINField1.setText(vehicle2.getVIN());
             v1YearField1.setText(vehicle2.getYear().toString());
@@ -144,6 +254,7 @@ public class SetDiscountsController {
         }
         
         if (vehicle3 != null) {
+            v1TimeLot2.setText(vehicle3.getTimeOnLot().toString());
             v1ValueField2.setText(vehicle3.getValue().toString());
             v1VINField2.setText(vehicle3.getVIN());
             v1YearField2.setText(vehicle3.getYear().toString());
@@ -155,7 +266,59 @@ public class SetDiscountsController {
             v1MechCon2.setText(vehicle3.getMechCondition());
             
         }
+        
+        if (vehicle4 != null) {
+            v1TimeLot3.setText(vehicle4.getTimeOnLot().toString());
+            v1ValueField3.setText(vehicle4.getValue().toString());
+            v1VINField3.setText(vehicle4.getVIN());
+            v1YearField3.setText(vehicle4.getYear().toString());
+            v1Make3.setText(vehicle4.getMake());
+            v1ModelField3.setText(vehicle4.getModel());
+            v1MileageField3.setText(vehicle4.getMileage().toString());
+            v1ColorField3.setText(vehicle4.getColor());
+            v1BodyCon3.setText(vehicle4.getBodyCondition());
+            v1MechCon3.setText(vehicle4.getMechCondition());
+            
+        }
        
+        if (vehicle5 != null) {
+            v1TimeLot4.setText(vehicle5.getTimeOnLot().toString());
+            v1ValueField4.setText(vehicle5.getValue().toString());
+            v1VINField4.setText(vehicle5.getVIN());
+            v1YearField4.setText(vehicle5.getYear().toString());
+            v1Make4.setText(vehicle5.getMake());
+            v1ModelField4.setText(vehicle5.getModel());
+            v1MileageField4.setText(vehicle5.getMileage().toString());
+            v1ColorField4.setText(vehicle5.getColor());
+            v1BodyCon4.setText(vehicle5.getBodyCondition());
+            v1MechCon4.setText(vehicle5.getMechCondition());
+        }
+        
+        if (vehicle6 != null) {
+            v1TimeLot5.setText(vehicle6.getTimeOnLot().toString());
+            v1ValueField5.setText(vehicle6.getValue().toString());
+            v1VINField5.setText(vehicle6.getVIN());
+            v1YearField5.setText(vehicle6.getYear().toString());
+            v1Make5.setText(vehicle6.getMake());
+            v1ModelField5.setText(vehicle6.getModel());
+            v1MileageField5.setText(vehicle6.getMileage().toString());
+            v1ColorField5.setText(vehicle6.getColor());
+            v1BodyCon5.setText(vehicle6.getBodyCondition());
+            v1MechCon5.setText(vehicle6.getMechCondition());
+        }
+        
+        if (vehicle7 != null) {
+            v1TimeLot6.setText(vehicle7.getTimeOnLot().toString());
+            v1ValueField6.setText(vehicle7.getValue().toString());
+            v1VINField6.setText(vehicle7.getVIN());
+            v1YearField6.setText(vehicle7.getYear().toString());
+            v1Make6.setText(vehicle7.getMake());
+            v1ModelField6.setText(vehicle7.getModel());
+            v1MileageField6.setText(vehicle7.getMileage().toString());
+            v1ColorField6.setText(vehicle7.getColor());
+            v1BodyCon6.setText(vehicle7.getBodyCondition());
+            v1MechCon6.setText(vehicle7.getMechCondition());
+        }
     }
     
     private void setVehicles() {
@@ -194,6 +357,36 @@ public class SetDiscountsController {
         carForDiscount = this.vehicle3;
     }
     
+    public void v4Save(ActionEvent event) throws IOException {
+        tabPane.setVisible(false);
+        discountPane.setVisible(true);
+        this.regularPrice.setText(vehicle4.getValue().toString());
+        carForDiscount = this.vehicle4;
+    }
+
+    public void v5Save(ActionEvent event) throws IOException {
+        tabPane.setVisible(false);
+        discountPane.setVisible(true);
+        this.regularPrice.setText(vehicle5.getValue().toString());
+        carForDiscount = this.vehicle5;
+    }
+
+    
+    public void v6Save(ActionEvent event) throws IOException {
+        tabPane.setVisible(false);
+        discountPane.setVisible(true);
+        this.regularPrice.setText(vehicle6.getValue().toString());
+        carForDiscount = this.vehicle6;
+    }
+
+    public void v7Save(ActionEvent event) throws IOException {
+        tabPane.setVisible(false);
+        discountPane.setVisible(true);
+        this.regularPrice.setText(vehicle7.getValue().toString());
+        carForDiscount = this.vehicle7;
+    }
+
+    
     public void discountSave(ActionEvent event) throws IOException {
         if (percentDiscount.getText() != "") {
             try {
@@ -228,6 +421,5 @@ public class SetDiscountsController {
         
         discountPane.setVisible(false);
         tabPane.setVisible(true);
-    }
-        
+    }    
 }
