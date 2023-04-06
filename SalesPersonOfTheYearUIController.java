fe9c5b61bb55c81bf7f9549a2e600fa45c313642
AtlanticRim <114529909+AtlanticRim@users.noreply.github.com>
2023-04-06 18:08:24 -0400

    Small update

--- SalesPersonOfTheYearUIController.java
+++ SalesPersonOfTheYearUIController.java
@@ -6,31 +6,31 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 
 public class SalesPersonOfTheYearUIController {
-	@FXML
-	private Button closeButton;
-	@FXML
-	private TextField salespersonNameTF;
-	@FXML
-	private Button calculateButton;
+    @FXML
+    private Button closeButton;
+    @FXML
+    private TextField salespersonNameTF;
+    @FXML
+    private Button calculateButton;
 
-	@FXML
-	public void close(ActionEvent event) throws IOException {
+    @FXML
+    public void close(ActionEvent event) throws IOException {
         //  goes back to the page.
-       	if(event.getSource() == closeButton) {
-       	    Main m = new Main();
-        //  m.changeScene(".fxml");
-       	}	
+        if(event.getSource() == closeButton) {
+            Main m = new Main();
+            m.changeScene(Main.getView());
+        }   
     }
-	
-	@FXML
-	public void salespersonName(ActionEvent event) throws IOException {
-	    //  Displays the salesperson name.
-		
-	}
-	
-	@FXML
-	public void calculate(ActionEvent event) throws IOException {
-		//  Calculates the salesperson of the year.
-		
-	}
+    
+    @FXML
+    public void salespersonName(ActionEvent event) throws IOException {
+        //  Displays the salesperson name.
+        
     }
+    
+    @FXML
+    public void calculate(ActionEvent event) throws IOException {
+        //  Calculates the salesperson of the year.
+        
+    }
+}
