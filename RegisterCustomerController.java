78836a9a52a81b38a9106e925bdd1f76b3ced57d
AtlanticRim <114529909+AtlanticRim@users.noreply.github.com>
2023-04-06 19:22:56 -0400

    Update to fix import issue
    Missing import statement for TextFormatter

--- RegisterCustomerController.java
+++ RegisterCustomerController.java
@@ -1,16 +1,13 @@
 package application;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.control.DatePicker;
-import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import java.io.IOException;
 import dao.CustomerDao;
 import backend.Address;
 import backend.CustomerProfile;
+import javafx.scene.control.TextFormatter;
 
 public class RegisterCustomerController {
     
