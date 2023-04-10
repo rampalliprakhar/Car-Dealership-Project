d30d7883a3b4cf12856edbdff614660dbc9b120a
AtlanticRim <114529909+AtlanticRim@users.noreply.github.com>
2023-04-09 22:23:50 -0400

    Update Mileage to Integer

--- SetDiscountsController.java
+++ SetDiscountsController.java
@@ -30,8 +30,8 @@ public class SetDiscountsController {
     @FXML
     private Button clearButton;
     
-    private Vehicle testCar1 = new Vehicle("123456789", new Double(15000.00), new Integer(1996), "Ford", "Fiesta", "New", "Broken", "Grey", 120000, new Date(System.currentTimeMillis()));
-    private Vehicle testCar2 = new Vehicle("987654321", new Double(17000), new Integer(2018), "Honda", "Acura", "New", "Broken", "Blue", 1500, new Date(System.currentTimeMillis()));
+    private Vehicle testCar1 = new Vehicle("123456789", new Double(15000.00), new Integer(1996), "Ford", "Fiesta", "New", "Broken", "Grey", new Integer(120000), new Date(System.currentTimeMillis()));
+    private Vehicle testCar2 = new Vehicle("987654321", new Double(17000), new Integer(2018), "Honda", "Acura", "New", "Broken", "Blue", new Integer(1500), new Date(System.currentTimeMillis()));
     private Vehicle testCar3 = new Vehicle();
     
     private Vehicle vehicle1 = null;
@@ -421,5 +421,8 @@ public class SetDiscountsController {
         
         discountPane.setVisible(false);
         tabPane.setVisible(true);
-    }    
+    }
+    
+    
+    
 }
