8e238e8ec471b134f12ba302493b1ec9cee97ae4
AtlanticRim <114529909+AtlanticRim@users.noreply.github.com>
2023-04-18 18:51:22 -0400

    Updated Comment
    Updated the comment for clarity

--- module-info.java
+++ module-info.java
@@ -1,4 +1,13 @@
+/* --------------------------------------------------- 
+ *  Author: Team 3 Car Dealership
+ *  Written: 2/15/23
+ *  Last Updated: 4/18/2023
+ *  
+ *  Eclipse requires this text to be inserted for the IDE to work with JavaFX & mySQL
+ ---------------------------------------------------*/
+
 module CarDealershipUI {
+
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.sql;
