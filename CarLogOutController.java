5b44bad2df259f476862dc192276df2cea2205db
AtlanticRim <114529909+AtlanticRim@users.noreply.github.com>
2023-04-18 18:59:23 -0400

    Added comments & cleaned up dead code

--- CarLogOutController.java
+++ CarLogOutController.java
@@ -1,38 +1,32 @@
+/* --------------------------------------------------- 
+ *  Author: Team 3 Car Dealership
+ *  Written: 2/01/23
+ *  Last Updated: 4/18/2023
+ *  
+ *  Compilation: javac CarLogOutController.java
+ *  Execution: java CarLogOutController
+ *  
+ *  Handles the logout process and screen.
+ *  Corresponding fxml file is CarLogOut.fxml
+ ---------------------------------------------------*/
+
 package application;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.*;
-import javafx.scene.Parent;
 import javafx.scene.control.*;
-import javafx.stage.Stage;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import java.io.IOException;
+
+
 public class CarLogOutController {
-     @ FXML
-     private Button LogoutButton;
-     private Stage stage;
-     private Scene scene;
-     private Parent root;
-    
-     public void LogOut(ActionEvent event) throws IOException {
+     
+    @FXML
+    private Button LogoutButton;
+   
+    public void LogOut(ActionEvent event) throws IOException {
          Platform.exit();
-         
-         
-         
-//         try {
-//            root = FXMLLoader.load(getClass().getResource("CarLogout.fxml"));
-//            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-//            scene = new Scene(root);
-//            stage.setScene(scene);
-//            stage.show();   
-//            LogoutButton.setOnAction(e -> {
-//              e.consume();
-//            }); 
-//         } catch(Exception e) {
-//            e.printStackTrace();
-//        }
-    }
- 
-}
+    } // end LogOut method
+    
+    
+ } // end class
