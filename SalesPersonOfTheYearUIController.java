0e624e87e190ee3a3b5f82bd90e851d30661e584
AtlanticRim <114529909+AtlanticRim@users.noreply.github.com>
2023-04-18 20:42:00 -0400

    Added comments
    Not functional yet, will need some DB code.

--- SalesPersonOfTheYearUIController.java
+++ SalesPersonOfTheYearUIController.java
@@ -1,4 +1,18 @@
+/* --------------------------------------------------- 
+ *  Author: Team 3 Car Dealership
+ *  Written: 2/13/23
+ *  Last Updated: 4/18/2023
+ *  
+ *  Compilation: javac SalesPersonOfTheYearUIController.java
+ *  Execution: java SalesPersonOfTheYearUIController
+ *  
+ *  Handles calculating Salesperson of the Year
+ *  If we had more time, it would also be the weekly reports screen.
+ *  Corresponding fxml file: SalesPersonOfTheYearUI.fxml
+ ---------------------------------------------------*/
+
 package application;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -6,6 +20,8 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 
 public class SalesPersonOfTheYearUIController {
+    
+    // Declare UI Fields
     @FXML
     private Button closeButton;
     @FXML
@@ -18,7 +34,7 @@ public class SalesPersonOfTheYearUIController {
 
     @FXML
     public void close(ActionEvent event) throws IOException {
-        //  goes back to the page.
+        //  goes back to the last page.
         if(event.getSource() == closeButton) {
             Main m = new Main();
             m.changeScene(Main.getView());
