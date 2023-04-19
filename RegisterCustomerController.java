f19bf06f90105995622d419314dd98911fadbf70
AtlanticRim <114529909+AtlanticRim@users.noreply.github.com>
2023-04-18 20:38:31 -0400

    Added comments & formatting

--- RegisterCustomerController.java
+++ RegisterCustomerController.java
@@ -1,4 +1,17 @@
+/* --------------------------------------------------- 
+ *  Author: Team 3 Car Dealership
+ *  Written: 1/30/23
+ *  Last Updated: 4/18/2023
+ *  
+ *  Compilation: javac RegisterCustomerController.java
+ *  Execution: java RegisterCustomerController
+ *  
+ *  Handles registering a new customer
+ *  Corresponding fxml file is RegCustomerUI.fxml
+ ---------------------------------------------------*/
+
 package application;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -8,10 +21,11 @@ import dao.CustomerDao;
 import backend.Address;
 import backend.CustomerProfile;
 import javafx.scene.control.TextFormatter;
-import javafx.scene.text.Text;
 
 public class RegisterCustomerController {
     
+    
+    // Declare UI Fields
     @FXML
     private TextField FirstNameField;
     @FXML
@@ -34,35 +48,25 @@ public class RegisterCustomerController {
     private Button ClearButton;
     @FXML
     private Button ReturnButton;
-	@FXML
-	private Text updateSuccessful, nullError;
     
     private String previousPage = Main.getView();
     
-	@FXML
+    @FXML
     public void initialize() {
-    	
-		// change name requirements bc they can have numbers and symbols????
-        // is 40 a good length limit?
-	    
-		// only allows alphabetical characters, dash, and apostrophe
+
+
+        // Length limit of 40
+		// only allows alphabetical characters, dash, and apostrophe of up to 40 characters
 		FirstNameField.setTextFormatter(new TextFormatter<> (change -> {
-			if (change.getControlNewText().length() == 1) {
-		    	change.setText(change.getText().toUpperCase());
-			}
 			if ((change.getControlNewText().length() > 40) ||
 				(change.getText().matches("[^a-zA-Z'-]"))) {
 				return null;
 			}
 			return change;
-		})); 
+		}));
 		
 		// only allows alphabetical characters, dash, and apostrophe of up to 40 characters
 		LastNameField.setTextFormatter(new TextFormatter<> (change -> {
-			 // guarentees first letter is capital
-			if (change.getControlNewText().length() == 1) {
-		    	change.setText(change.getText().toUpperCase());
-			}
 			if ((change.getControlNewText().length() > 40) ||
 				(change.getText().matches("[^a-zA-Z'-]"))) {
 				return null;
@@ -72,27 +76,26 @@ public class RegisterCustomerController {
 		
 		// only allows numbers, parenthesis, and dashes
 		PhoneField.setTextFormatter(new TextFormatter<> (change -> {
-			if ((change.getControlNewText().length() > 10) ||
-				(change.getText().matches("[^0-9]"))) {
+			if (change.getText().matches("[^0-9-()]")) {
 				return null;
 			}
 			return change;
-		}));
-				
-		// only allows numbers and uppercase characters up to 16
+		})); // not correct, add length limit
+			
+			
+		// only allows numbers and uppercase characters up to 12
 		DriversLicenseField.setTextFormatter(new TextFormatter<> (change -> {
-				change.setText(change.getText().toUpperCase());					
-				if ((change.getControlNewText().length() > 16) ||
-				(change.getText().matches("[^A-Z0-9]"))) {
+				if ((change.getControlNewText().length() > 12) ||
+				(change.getText().matches("[^A-Z1-9]"))) {
 				return null;
 			}
 			return change;
-		})); // length limit correct?
+		})); // change length limit NOWWWWW
 		
 		// only allows numbers and regular characters up to 40
 		AddressField.setTextFormatter(new TextFormatter<> (change -> {
 			if ((change.getControlNewText().length() > 40) ||
-			(change.getText().matches("[^A-Za-z0-9\\s]"))) {
+			(change.getText().matches("[^A-Za-z1-9\\s]"))) {
 			return null;
 		}
 		return change;
@@ -131,45 +134,15 @@ public class RegisterCustomerController {
         
         /* This method will also need to save all of the inputs to the
          * newly created Customer object.*/
-    	
-    	// input validation
-    	
-    	// if any fields are empty return and print out error message
-    	if (DriversLicenseField.getText().length() == 0 || FirstNameField.getText().length() == 0 || 
-    			LastNameField.getText().length() == 0 || PhoneField.getText().length() == 0 || 
-    			AddressField.getText().length() == 0 || CityField.getText().length() == 0 || 
-    			StateField.getText().length() == 0 || ZIPField.getText().length() == 0) {
-        	
-    		updateSuccessful.setText(null);
-    		nullError.setText("*Error: Please fill out all input fields*");
-    		return;
-    	}
-    	
-    	// if zip is less than 5 digits print out error message
-    	if (ZIPField.getText().length() < 5) {
-    		updateSuccessful.setText(null);
-    		nullError.setText("*Error: Please input a valid zip code*");
-    		return;	
-    	}
-
-    	// if phone number is less than 10 or a series of a repeating number print out error message
-    	if (PhoneField.getText().length() < 10 || validPhoneNum() == false) {
-    		updateSuccessful.setText(null);
-    		nullError.setText("*Error: Please input a valid phone number*");
-    		return;	
-    	}
-    	    	
-    	// end input validation
-    	        
+        
         Address custAddress = new Address(AddressField.getText(), CityField.getText(), StateField.getText(), ZIPField.getText(), "");
         
-        custAddress.printAddress(); // DEBUG
+        custAddress.printAddress();
         
-        String licenseAsString = DriversLicenseField.getText();
-        Long licenseNum = null;
-        if (licenseAsString != null && licenseAsString.length() > 0) licenseNum = Long.decode(licenseAsString);
-        
-        CustomerProfile customer = new CustomerProfile(licenseNum, FirstNameField.getText(), LastNameField.getText(), PhoneField.getText(), custAddress);
+        /* Creates a new Customer with the information and a new DAO class.
+         * Utilizes the DAO class to save the customer to the DB
+         */
+        CustomerProfile customer = new CustomerProfile(DriversLicenseField.getText(), FirstNameField.getText(), LastNameField.getText(), PhoneField.getText(), custAddress);
         CustomerDao dao = new CustomerDao();
         
         System.out.println(customer.toString());
@@ -180,12 +153,15 @@ public class RegisterCustomerController {
             System.out.println("Error in RegisterCustomerController.java");
         }
         
+        // Sends user back to main screen once save is completed
         Main m = new Main();
         m.changeScene(previousPage);
         
     } // end save
     
     public void clear(ActionEvent event) throws IOException {
+        
+        // Clears and resets all data fields.
         
         FirstNameField.clear();
         LastNameField.clear();
@@ -199,26 +175,11 @@ public class RegisterCustomerController {
     } // end clear
     
     public void pageReturn(ActionEvent event) throws IOException {
+        // Takes users back to previous page       
         
         Main m = new Main();
         m.changeScene(previousPage);
         
     } // end pageReturn
-	
-    // receives information from the search customer UI
-    public void showInformation(String firstName, String lastName) {
-    	FirstNameField.setText(firstName);
-    	LastNameField.setText(lastName);
-    }
-    
-    // input validation: returns false if phone number is series of repeating number
-    public boolean validPhoneNum() {
-    	char a = PhoneField.getText().charAt(0);
-    	for (int i = 1; i < PhoneField.getText().length(); i++) {
-    	if (PhoneField.getText().charAt(i) != a)
-    		return true;
-    	}
-    	return false;
-    }
     
 }
