ca931e6e51583d5de630fdeb8caf911371c07774
TrinyN <122491610+TrinyN@users.noreply.github.com>
2023-04-20 20:03:19 -0400

    deleted view, combined two methods, integrate dao

--- SearchCustomerController.java
+++ SearchCustomerController.java
@@ -1,77 +1,62 @@
 package application;
 
 import java.io.IOException;
+import dao.CustomerDao;
 import java.time.LocalDate;
-import javafx.collections.ObservableList;
+import backend.CustomerProfile;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.control.*;
 import javafx.scene.*;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.text.Text;
+
 
 public class SearchCustomerController {
+	
+	private CustomerProfile customer;
+
+    @FXML
+    private Text errorText;
+    
     @FXML
     private ChoiceBox<String> makeDropdown, paymentMethod;
     
     @FXML
     private DatePicker salesDate;
-
-	
-	private CustomerProfile selectedCus;
 	
     @FXML
-    private TextField firstName, lastName, VINField, yearField, modelField, valueField, tempFirst, tempLast, tempID, realCusID;
+    private TextField customerID, VINField, yearField, modelField, valueField, tempFirst, tempLast, tempID, actualCusID;
     
     @FXML
     private Button searchButton, clearButton, returnButton, registerNewCustomer;
-    
-//    private String previousPage = Main.getView();
-
-    
-    @FXML
-    private ListView<String> listView;
-    
+        
     @FXML
     public void initialize() {    	
     	
-		// only allows alphabetical characters, dash, and apostrophe
-		firstName.setTextFormatter(new TextFormatter<> (change -> {
-			if (change.getControlNewText().length() == 1) {
-		    	change.setText(change.getText().toUpperCase());
-			}
-			if ((change.getControlNewText().length() > 40) ||
-				(change.getText().matches("[^a-zA-Z'-]"))) {
-				return null;
-			}
-			return change;
-		}));
-		
-		// only allows alphabetical characters, dash, and apostrophe
-		lastName.setTextFormatter(new TextFormatter<> (change -> {
-			if (change.getControlNewText().length() == 1) {
-		    	change.setText(change.getText().toUpperCase());
-			}
-			if ((change.getControlNewText().length() > 40) ||
-				(change.getText().matches("[^a-zA-Z'-]"))) {
-				return null;
-			}
-			return change;
-		})); 	
+    	// Text Formatter Input Validation
+		// only allows numbers and uppercase characters up to 16
+		customerID.setTextFormatter(new TextFormatter<> (change -> {
+			change.setText(change.getText().toUpperCase());					
+			if ((change.getControlNewText().length() > 16) ||
+			(change.getText().matches("[^A-Z0-9]"))) {
+			return null;
+		}
+		return change;
+	})); // length limit correct?	
     }
         
     public void clear(ActionEvent event) {
         
         /* This method clears all of the fields.*/
         
-        firstName.clear();
-        lastName.clear();
+        customerID.clear();
         
     } // end clear
     
     public void pageReturn(ActionEvent event) throws IOException {
     	
-        /* This method returns to the record of sale UI while retaining the information of the data filled.*/
+        /* This method returns to the record of sale UI while retaining the information of the filled data.*/
 
     	FXMLLoader loader = new FXMLLoader(getClass().getResource("RecordOfSaleUI.fxml"));
     	Parent root = loader.load();
@@ -83,71 +68,63 @@ public class SearchCustomerController {
     	m.changeScene("RecordOfSaleUI.fxml", root);
     } // end pageReturn
     
-    public void searchCus(ActionEvent event) throws IOException {    	
+    public void searchCus(ActionEvent event) throws IOException {   
     	
-    	// listView.setItems(list); make list of all customer from database with inputed name?
+    	// if nothing is typed, return and print message to the user
+    	if (customerID.getText().isBlank()) {
+    		errorText.setText("*Please input a driver's license number*");
+    		return;
+    	}
     	
-    	// database needed: search database and get name, DOB, and ID
-    	
-    	
-    	// test data
-    	String fullName = firstName.getText() + " " + lastName.getText();
-    	String DOB = "08/23/2003";
-    	int customerID = 5092; // should ID be string to allow a 0 in the front ex: 0873
-
-      for (int i = 0; i < 4; i++) {
-      	listView.getItems().addAll("Name:    " + fullName +"\n" + 
-      							   "DOB:      " + DOB + "\n" + 
-      							   "ID:          " + customerID);
-
-      	
-//      ObservableList<String> list = FXCollections.observableArrayList("Mark Zuckerburg \n10/09/2000", "Tom Clancy\n02/06/1999", "Jack Sparrow\n03/18/1989"); 
-    	 
-//    	listView.setItems(list);
-      }
+        try {
+        	// searches database for customer with inputed customer ID and sets it to this.customer
+            CustomerDao dao = new CustomerDao();            
+            this.customer = dao.retriveCustomer(customerID.getText());
+            
+            // if customer is found open customer profile UI while passing customer ID and other additional information
+            if (customer.getLicenseNum() != null) {
+        	FXMLLoader loader = new FXMLLoader(getClass().getResource("CustomerProfileUI.fxml"));
+        	Parent root = loader.load();
+        	
+        	CustomerProfileController controller = loader.getController();
+        	controller.showInformation(customer.getLicenseNum(), yearField.getText(), makeDropdown.getValue(), modelField.getText(), VINField.getText(), valueField.getText(), paymentMethod.getValue(), salesDate.getValue());
+        	
+        	Main m = new Main();
+        	m.changeScene("CustomerProfileUI.fxml", root);
+        	        	
+            }
+            
+            // if customer if not found, print error message and prompt user to register customer
+            else { 
+            	errorText.setText("Customer Not Found, Please Register");
+            }
+            
+        } catch (Exception e) {
+    		System.out.println("Error in SearchCustomerController.java");
+        	return;
+        }
       
-    } // not complete
-
-    
-	@FXML
-	// sends any inputed data in record of sale to customer profile UI
-    public void openCusProfile(MouseEvent event) throws IOException{
-		
-        /* This method opens the customer profile page of the selected customer.*/
-
-		// test data
-		realCusID.setText("123456789"); // should get id of selected customer
-		
-    	FXMLLoader loader = new FXMLLoader(getClass().getResource("CustomerProfileUI.fxml"));
-    	Parent root = loader.load();
-    	
-    	CustomerProfileController controller = loader.getController();
-    	controller.showInformation(realCusID.getText(), yearField.getText(), makeDropdown.getValue(), modelField.getText(), VINField.getText(), valueField.getText(), paymentMethod.getValue(), salesDate.getValue());
-    	
-    	Main m = new Main();
-    	m.changeScene("CustomerProfileUI.fxml", root);
-    } // end openCusProfile
+    } // needs testing
 	
-	// sends first and last name to register customer page
+	// opens RegisterCustomerUI and passes inputed customer ID
     public void registerNewCus(ActionEvent event) throws IOException {
     	
     	FXMLLoader loader = new FXMLLoader(getClass().getResource("RegisterCustomerUI.fxml"));
     	Parent root = loader.load();
     	
     	RegisterCustomerController regCusController = loader.getController();
-    	regCusController.showInformation(firstName.getText(), lastName.getText());
+    	regCusController.showInformation(customerID.getText());
     	
     	Main m = new Main();
     	m.changeScene("RegisterCustomerUI.fxml", root);
     } // end registerNewCus
     
-    // receives first and last name from customer profile UI
-    public void showInformation(String firstName, String lastName) {
-    	this.firstName.setText(firstName);
-    	this.lastName.setText(lastName);
+    // receives customer ID from customer profile UI
+    public void showInformation(String customerID) {
+    	this.customerID.setText(customerID);
     }
     
-    // receives car information from record of sales UI
+    // receives recorded sale information from record of sales UI
     public void showInformation(String first, String last, String cusID, String year, String make, String model, String VIN, String price, String paymentMethod, LocalDate salesDate) {
     	tempFirst.setText(first);
     	tempLast.setText(last);
